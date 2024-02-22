8db27d808957c4ca9b9cdc0a764d99e13c7ffb1d
Kevin <dukso123@gmail.com>
2024-02-22 17:24:27 +0900

    Google API find list of places

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine, inspect
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
+from wtforms import validators, StringField, SubmitField, URLField, IntegerField, TimeField, SelectField
 import requests
 import os
 from geocode import GetGeo
+from pprint import pprint
 
 # ===== App Setting =====
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///cafes.db"
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # set to reduce memory usage
 db = SQLAlchemy(app)
 Bootstrap(app)
@@ -34,7 +37,26 @@ class Cafe(db.Model):
 
 
 class CafeForm(FlaskForm):
-    cafe_name = None
+    location = StringField(label='Where do You Live?',
+                           validators=[validators.DataRequired()],
+                           render_kw={'placeholder': '경기도 성남시 분당구 정자일로 95',
+                                      'class': 'form-control'})
+    type = SelectField(label='Place Type',
+                       default='cafe',
+                       validators=[validators.DataRequired()],
+                       choices=[('restaurant', 'Restaurant'), ('cafe', 'Café'), ('bar', 'Bar'),
+                                ('hospital', 'Hospital'), ('atm', 'Atm'), ('museum', 'Museum'), ('park', 'Park'),
+                                ('gas_Station', 'Gas Station'), ('supermarket', 'Supermarket')],
+                       render_kw={'class': 'form-control'})
+    keyword = StringField(label="Place Keyword",
+                          render_kw={"placeholder": '메가커피',
+                                     'class': 'form-control'})
+    radius = IntegerField(label='Radius in m²',
+                          validators=[validators.DataRequired()],
+                          render_kw={'placeholder': '500',
+                                     'class': 'form-control'})
+    submit = SubmitField(label='Find',
+                         render_kw={'class': 'btn btn-warning form-control'})
 
 
 def create_db():
@@ -43,25 +65,40 @@ def create_db():
         db.create_all()
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     key = os.getenv('GOOGLE_API_KEY')
-    print(key)
-    location_name = "분당"
+
+    # Flask Form
+    form = CafeForm()
+    location, key, result = None, None, []
+    if form.validate_on_submit():
+        location = form.location.data
+        type_ = form.type.data
+        keyword = form.keyword.data
+        radius = form.radius.data
+        print(f"{location}, {type_}, {keyword}, {radius}")
+        gg = GetGeo()
+        result = gg.by_geo(type_=type_, keyword=keyword, address=location, radius=radius, save=True)
+        result = result['results']
+        print(f"result: {result}")
     return render_template('index.html',
-                           location_name=location_name,
-                           key=key)
+                           form=form,
+                           location=location,
+                           key=key,
+                           result=result)
+
 
 @app.route('/findcafes', methods=['GET'])
 def find_cafes():
-    print(gg.get_lat_long())
-    name = request.args.get('name')
     radius = 500
     type = 'cafe'
-    location = gg.get_lat_long('경기도 성남시 분당구 정자동 84-6')
-    print(location)
+    location = '경기도 성남시 분당구 서현동'
+    lat_long = gg.get_lat_long(location)
+    print(f"location: {location}, lat_long: {lat_long}")
     key = os.getenv('GOOGLE_API_KEY')
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={location[0]},{location[1]}&radius={radius}&type={type}&key={key}"
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat_long[0]},{lat_long[1]}&radius={radius}&type={type}&key={key}"
+    print(url)
     response = requests.get(url)
     if response.status_code == 200:
         return jsonify(response.json())
--- geocode.py
+++ geocode.py
@@ -12,35 +12,72 @@ load_dotenv()
 class GetGeo():
     def __init__(self):
         self.url = "https://maps.googleapis.com/maps/api/geocode/json?"
-        self.data = None
+        self.url_nearby = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?'
+        self.key = os.getenv('GOOGLE_API_KEY')
 # ===== Get Place Geocode =====
-    def get_geo(self, address: str = '서울', filepath: str = 'geo.json', save: bool = False) -> Optional[dict]:
-        """returns api response as json from Google place API
-            save as file if save param set as True"""
+    def get_geo(self, address: str = 'Jeju-Do', filepath: str = 'geo.json', save: bool = False) -> Optional[list]:
+        """
+        :param address: str address of the place to search default='Jeju-Do
+        :param filepath: file name for the json to be saved as default='geo.json'
+        :param save: saves if set True default=False
+        :return: list ['lat', 'lng']
+        """
         params = {
             "address": address,
-            'key': os.environ.get('GOOGLE_API_KEY')
+            'key': self.key
                 }
-        response = requests.get(self.url, params=params)
+        response = requests.get(url=self.url, params=params)
         if response.status_code == 200:
-            self.data = response.json()
+            data = response.json()
             if save:
                 try:
                     with open(filepath, 'r') as file:
                         existing_data = json.load(file)
-                        print(existing_data)
+                        print(f"existing_data: {existing_data}")
                 except (FileNotFoundError, JSONDecodeError):
                     existing_data = []
-                existing_data.append(self.data)
+                existing_data.append(data)
                 with open(filepath, 'w') as file:
                     json.dump(existing_data, file, indent=4)
-            return self.data
+            return data
         else:
             return None
 
-    def get_lat_long(self, address: str='서울') -> list:
-        """:returns [lat, long] list"""
-        self.data = self.get_geo(address)
-        lat = self.data['results'][0]['geometry']['viewport']['southwest']['lat']
-        long = self.data['results'][0]['geometry']['viewport']['southwest']['lng']
-        return [lat, long]
+    def by_geo(self, filepath: str = 'place.json', save: bool = False, address: str ='Jeju-Do', keyword: str = None, radius: int=500, type_: str='cafe') -> Optional[dict]:
+        """
+        :param filepath str filepath name  default='place.json'
+        :param save set True to save response json as file, default=False
+        :param address place to search for, passes to get_geo()
+        :param keyword what to find in the address radius
+        :param radius search radius by m² default=500
+        :param type_ type of place to search e.g., 'cafe', 'restaurant'
+        :returns api response as json from Google place API, save as file if save param set as True"""
+        data = self.get_geo(address, save=True)
+        print(f"data from get_geo: {data}")
+        try:
+            geo_list = [
+                        data['results'][0]['geometry']['viewport']['southwest']['lat'],
+                        data['results'][0]['geometry']['viewport']['southwest']['lng']
+                        ]
+            params = {
+                'location': f"{geo_list[0]},{geo_list[1]}",
+                'radius': radius,
+                'type': type_,
+                'key': self.key,
+                'keyword': keyword
+            }
+            response = requests.get(url=self.url_nearby, params=params).json()
+            print(response)
+        except IndexError:
+            response = {'results': []}
+
+        if save:
+            try:
+                with open(filepath, 'r') as file:
+                    existing_data = json.load(file)
+            except (FileNotFoundError, JSONDecodeError):
+                existing_data = []
+            existing_data.append(data)
+            with open(filepath, 'w') as file:
+                json.dump(existing_data, file, indent=4)
+        return response
